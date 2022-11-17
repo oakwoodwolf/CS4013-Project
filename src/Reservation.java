7389c08e3fbf29ac02b115b1c14f936c88ebd050
nikolettakozma <kozmanikoletta05@gmail.com>
2022-11-17 12:25:52 +0000

    Reservation Added

---
+++ src/Reservation.java
@@ -0,0 +1,37 @@
+package src;
+import java.sql.Time;
+import java.util.Date;
+
+public class Reservation {
+    public String name;
+    public int PhoneNumber;
+    public Date LocalDate;
+    public Time LocalTime;
+
+    public int CustomerId;
+    public int NoOfPeople; // Number of people
+    int TableId; // table number
+
+    public static void main(String[] args) {
+    }
+
+    public void MakeReservation(String Name, int CustomerID, int noOfPeople, int phoneNumber, int tableId) {
+        this.name = Name;
+        this.CustomerId = CustomerID;
+        this.NoOfPeople = noOfPeople;
+        this.PhoneNumber = phoneNumber;
+        this.TableId = tableId;
+    }
+
+    public int getCustomerID() {
+        return this.CustomerId;
+    }
+    public Date getDate() {
+        return LocalDate;
+    }
+    public String toString() {
+        return name + "\t" + CustomerId + "\t" + PhoneNumber + "\t" + NoOfPeople + "\t";
+    }
+
+
+}
